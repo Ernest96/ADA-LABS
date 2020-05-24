7fd88f152049d172d9166e3dc51c8d5e2306b325
Ernest96 <bitca@inbox.ru>
2020-05-24 16:59:50 +0300

    Started work on LAB2 (NodeJS + CSharp)

---
+++ ADA-LAB2/C-Sharp/LAB2/Common/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class Settings
+    {
+        public static string RabbitMQHost = "localhost";
+        public static string QueueName = "ADA-LAB2";
+    }
+}
---
+++ ADA-LAB2/C-Sharp/LAB2/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = Settings.RabbitMQHost };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: Settings.QueueName, false, false, false, null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", Settings.QueueName, null, body);
+                Console.WriteLine("[x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
