4c26529fb6b12e5c8950c0590fd6d75e609de26e
Ernest96 <bitca@inbox.ru>
2020-05-25 15:44:06 +0300

    Lab2 node.js code cleanup

--- ADA-LAB2/node/receiver.js
+++ ADA-LAB2/node/receiver.js
@@ -3,14 +3,15 @@
 var fibonacciCalculator = require('./fibonacciCalculator');
 var amqp = require('amqplib/callback_api');
 var fileStream = require('fs');
-const QUEUE = 'ADA-LAB2';
-const OUTPUT = "D:\\Programming\\master\\ADA\\ADA-LABS\\ADA-LAB2\\output.txt";
+
+const queueName = 'ADA-LAB2';
+const output = "D:\\Programming\\master\\ADA\\ADA-LABS\\ADA-LAB2\\output.txt";
 
 var consumeMessage = async function(msg) {
     var number = msg.content.toString();
     console.log("[x] Received " + number);
     var result = await fibonacciCalculator.sleepyFibonacci(number);
-    fileStream.appendFile(OUTPUT, result + "\n", function (err) {
+    fileStream.appendFile(output, result + "\n", function (err) {
         if (err) throw err;
       });
 }
@@ -25,10 +26,10 @@ amqp.connect('amqp://localhost', function(connectError, connection) {
             throw channelError;
         }
 
-        channel.assertQueue(QUEUE, { durable: false });
+        channel.assertQueue(queueName, { durable: false });
 
-        console.log("[*] Waiting for messages in %s. To exit press CTRL+C", QUEUE);
+        console.log("[*] Waiting for messages in %s. To exit press CTRL+C", queueName);
 
-        channel.consume(QUEUE, consumeMessage, { noAck: true });
+        channel.consume(queueName, consumeMessage, { noAck: true });
     });
 });
